9caf6240d553f939095d07138ca421558104baf2
Pojemnik <andriej194@gmail.com>
2019-12-17 19:36:31 +0100

    Reading example xml works now

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <tinyxml2.h>
 #include <fstream>
+#include <string>
 
 int main()
 {
-	//std::fstream file;
-	//file.open("C:\PulpitS\WorkingDir\StorkMan\stork_map_example.xml");
 	tinyxml2::XMLDocument map;
 	map.LoadFile("stork_map_example.xml");
-	map.FirstChildElement("level")->FirstChildElement("plaform")->FindAttribute("position");
+	std::string e = map.FirstChildElement("level")->FirstChildElement("platform")->Attribute("position");
 	return 0;
 }
