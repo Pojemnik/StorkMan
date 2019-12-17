8968466c918811a67494b744a6c75010a208b969
Pojemnik <andriej194@gmail.com>
2019-12-17 21:26:44 +0100

    Added map files

--- main.cpp
+++ main.cpp
@@ -2,10 +2,16 @@
 #include <tinyxml2.h>
 #include <fstream>
 #include <string>
+#include "map.h"
 
-int main(int argc, char** argv)
+int main(int argc, char** argv)	//Second argument is a map file for editor
 {
 	tinyxml2::XMLDocument map;
-	map.LoadFile("stork_map_example.xml");
+	map.LoadFile(argv[1]);
+	tinyxml2::XMLNode* root = map.FirstChild();
+	if (root == NULL)
+	{
+		std::cout << "B³¹d w pierwszym elemencie pliku!" << std::endl;
+	}
 	return 0;
 }
