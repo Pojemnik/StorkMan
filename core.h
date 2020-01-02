822ed299145ba29b8003caae2eba0ddcc2155d39
Pojemnik <andriej194@gmail.com>
2020-01-02 23:35:04 +0100

    Example classes

--- core.h
+++ core.h
@@ -1,18 +1,29 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+
+typedef sf::Vector2f Vectorf;
+typedef sf::Vector2i Vectori;
 
 class Transformable
 {
+public:
 	virtual void move() = 0;
+private:
+	Vectorf local_position;
 };
 
 class Animatable
 {
+public:
 	virtual void update() = 0;
+	//Jak bêdzie potrzebne to siê napisze
 };
 
 class Drawable
 {
+public:
 	virtual void draw() = 0;
+	//Jak bêdzie potrzebne to siê napisze
 };
 
 class Colidable
