0fc56924f032f7257c9dbe84f9fcd37675822c11
Pojemnik <andriej194@gmail.com>
2020-02-14 21:46:20 +0100

    Added new graphics and loading them

--- assets.cpp
+++ assets.cpp
@@ -12,4 +12,49 @@ void Assets::loadAssets()
 		}
 		bricks[i].setRepeated(true);
 	}
+	panels.reserve(3);
+	for (int i = 0; i < 3; i++)
+	{
+		panels.push_back(sf::Texture());
+		if (!panels[i].loadFromFile("img/tex/PANELE0" + std::to_string(i) + ".PNG"))
+		{
+			std::cout << "Error PANELE0" + std::to_string(i) + ".png" << std::endl;
+		}
+		panels[i].setRepeated(true);
+	}
+	metal.reserve(3);
+	for (int i = 0; i < 3; i++)
+	{
+		metal.push_back(sf::Texture());
+		if (!metal[i].loadFromFile("img/tex/ZEBROWANIE0" + std::to_string(i) + ".PNG"))
+		{
+			std::cout << "Error ZEBROWANIE0" + std::to_string(i) + ".png" << std::endl;
+		}
+		metal[i].setRepeated(true);
+	}
+	if (!asphalt.loadFromFile("img/tex/ASFALT.png"))
+	{
+		std::cout << "Error ASFALT.png" << std::endl;
+	}
+	asphalt.setRepeated(true);
+	if (!concrete.loadFromFile("img/tex/BETON.png"))
+	{
+		std::cout << "Error BETON.png" << std::endl;
+	}
+	concrete.setRepeated(true);
+	if (!ground.loadFromFile("img/tex/ZIEMIA.png"))
+	{
+		std::cout << "Error ZIEMIA.png" << std::endl;
+	}
+	ground.setRepeated(true);
+	if (!grass.loadFromFile("img/tex/TRAWA.png"))
+	{
+		std::cout << "Error TRAWA.png" << std::endl;
+	}
+	grass.setRepeated(true);
+	if (!rivet.loadFromFile("img/tex/NITY00.png"))
+	{
+		std::cout << "Error NITY00.png" << std::endl;
+	}
+	rivet.setRepeated(true);
 }
--- assets.h
+++ assets.h
@@ -4,6 +4,13 @@
 struct Assets
 {
 	std::vector<sf::Texture> bricks;
+	std::vector<sf::Texture> panels;
+	std::vector<sf::Texture> metal;
+	sf::Texture asphalt;
+	sf::Texture concrete;
+	sf::Texture ground;
+	sf::Texture grass;
+	sf::Texture rivet; //nit
 
 	void loadAssets();
 };
