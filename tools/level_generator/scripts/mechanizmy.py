d71d7878ab3d42525648aee06da9a673a51a4ddc
Gahlammon <71130979+Gahlammon@users.noreply.github.com>
2020-09-15 18:54:59 +0200

    New structure in Level Generator
    mechanizmy.taśmociąg_jednostronny

--- tools/level_generator/scripts/mechanizmy.py
+++ tools/level_generator/scripts/mechanizmy.py
@@ -1,18 +1,35 @@
+import math
 global lvl
-def taśmociąg_jednostronny(x=0, y=0, długość=10, gruość=0.5, prędkość=2, segmentowanie=10, tekstura="panels,0", elementy=(0,0))
+def taśmociąg_jednostronny(x=0, y=0, długość=10, grubość=0.5, obrót=0, prędkość=2, segmentowanie=10, bariery=1, widoczność_barier=1, tekstura="panels,0", elementy=(0,0)):
     czas = round(długość/segmentowanie/prędkość*60)
-    for i in range(segmentowanie):
+    długość_x = math.cos(math.radians(obrót))*długość/segmentowanie
+    grubość_x = math.cos(math.radians(90-obrót))*grubość
+    długość_y = math.sin(math.radians(obrót))*długość/segmentowanie
+    grubość_y = -math.sin(math.radians(90-obrót))*grubość
+    for i in range(segmentowanie-1):
         lvl.add("""
-        <platform position="{0},{1}" texture="{2}">
-            <p></p>
-            <p></p>
-            <p></p>
+        <linear_platform position="{0},{1}" texture="{2}">
+            <p>0,0,{3}</p>
+            <p>{4},{5},0</p>
+            <p>{6},{7},{8}</p>
 
             <v>0,0</v>
-            <v>{3},0</v>
-            <v>{3},{4}</v>
-            <v>0,{4}</v>
-        </platform>""".format(x, y, tekstura_podpórki, grubość*obrót, długość))
-
-
-    
+            <v>{9},{10}</v>
+            <v>{11},{12}</v>
+            <v>{13},{14}</v>
+        </linear_platform>""".format(x+długość_x*i, y+długość_y*i, tekstura, (segmentowanie-1-i)*czas, długość_x*(segmentowanie-1-i), długość_y*(segmentowanie-1-i), -długość_x*i, -długość_y*i, i*czas, długość_x, długość_y, długość_x+grubość_x, długość_y+grubość_y, grubość_x, grubość_y))
+    if bariery == 1:
+        lvl.add("""
+        <platform position="{0},{1}" texture="{2}" visible="{3}">
+            <v>0,0</v>
+            <v>{4},{5}</v>
+            <v>{6},{7}</v>
+            <v>{8},{9}</v>
+        </platform>""".format(x+długość_x*(segmentowanie-1), y+długość_y*(segmentowanie-1), tekstura, widoczność_barier, długość_x, długość_y, długość_x+grubość_x, długość_y+grubość_y, grubość_x, grubość_y))
+        lvl.add("""
+        <platform position="{0},{1}" texture="{2}" visible="{3}">
+            <v>0,0</v>
+            <v>{4},{5}</v>
+            <v>{6},{7}</v>
+            <v>{8},{9}</v>
+        </platform>""".format(x, y, tekstura, widoczność_barier, długość_x, długość_y, długość_x+grubość_x, długość_y+grubość_y, grubość_x, grubość_y))
