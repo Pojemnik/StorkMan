699c6145d8a1909613e1db918c3e3e8845fa99b6
Pojemnik <andriej194@gmail.com>
2020-11-14 11:32:31 +0100

    Color genrerator shuffles colors now

--- tools/colors/color_generator.py
+++ tools/colors/color_generator.py
@@ -2,6 +2,7 @@
 #Arguments:
 # - Step for color change (1-255)
 import sys
+import random
 
 if len(sys.argv) < 2:
     print('Error: Not enough arguments.')
@@ -9,16 +10,17 @@ if len(sys.argv) < 2:
 if len(sys.argv) > 3:
     print('Error: Too many arguments.')
     quit()
-if sys.argv[1] < 1 or sys.argv[1] > 255:
+step = int(sys.argv[1])
+if step < 1 or step > 255:
     print('Error: Incorrect argument.')
     quit()
 colors = []
 top = 255
-step = int(sys.argv[1])
 for r in range(0, top, step):
     for g in range(0, top, step):
         for b in range(0, top, step):
             colors.append((str(r), str(g), str(b)))
+random.shuffle(colors)
 with open('colors.txt', 'w') as f:
     for c in colors:
         f.write('{} {} {}\n'.format(c[0], c[1], c[2]))
